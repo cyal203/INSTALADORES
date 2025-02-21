e10222f8c7a57ee5c235f5cb0033a62e4d98f1c3
cyal203 <93794670+cyal203@users.noreply.github.com>
2025-02-21 16:02:36 -0300

    Update DIGITACAO.cmd

--- SP/DIGITACAO.cmd
+++ SP/DIGITACAO.cmd
@@ -13,27 +13,15 @@ echo.
 ren "C:\Program Files (x86)\Fenox V1.0" "Fenox V1.0.OLD"
 cls
 timeout /t 2 /nobreak >nul
-REM ******************* BAIXA A NOVA VERSAO ****************
-echo   ════════════════════════════════════
-echo   ███ BAIXANDO ARQUIVOS (1/4)      ███
-echo   ════════════════════════════════════ 
 echo Efetuando Download da versao 1.0.0.1...
 curl -g -k -L -# -o "%temp%\1.0.0.1.zip" "https://www.dropbox.com/scl/fi/tkdbcsgdihazugpyft7ei/1.0.0.1.zip?rlkey=22girigvqgqu0e1y9akhcl82q&st=i63hturk&dl=1" >nul 2>&1
 cls
-
 REM ******************* EXTRAI NOVO SISOCR ****************
 cls
-echo   ════════════════════════════════════
-echo   ███   EXTRAINDO ARQUIVOS (2/4)  ███
-echo   ════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
 powershell -NoProfile Expand-Archive '%temp%\1.0.0.1.zip' -DestinationPath '%temp%\Fenox' >nul 2>&1 
-
 REM ******************* INSTALANDO ****************
 cls
-echo   ════════════════════════════════════
-echo   ███    INSTALANDO . . . . (3/4)  ███
-echo   ════════════════════════════════════ 
 timeout /t 2 /nobreak >nulf
 %temp%\Fenox\Fnx_1.0.0.1_x64.exe /silent
 timeout /t 2 /nobreak >nul
@@ -43,13 +31,10 @@ rmdir /s /q "C:\Program Files (x86)\Fenox V1.0.OLD"  >nul
 del /f "C:\Program Files (x86)\Fenox V1.0\notasAtualizacao.html"  >nul
 timeout /t 2 /nobreak >nul
 cls
-echo   ════════════════════════════════════
-echo   ███   FINALIZADO . . . .    (4/4)███
-echo   ════════════════════════════════════ 
 timeout /t 2 /nobreak >nul
 echo.
 echo Fenox V1
-wmic datafile where name="C:\\Program Files (x86)\\Fenox V1.0\\Fnx64bits.exe" get Version || goto :INSTALAR >nul
+wmic datafile where name="C:\\Program Files (x86)\\Fenox V1.0\\Fnx64bits.exe" get Version
 REM ******************* CRIA ATALHOS ****************
 rem powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'Fenox V1.0.lnk')); $s.TargetPath = 'C:\Program Files (x86)\Fenox V1.0\SisFnxUpdate.exe'; $s.Save()"
 rem powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut([System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'Fenox V1.0 Mobile.lnk')); $s.TargetPath = 'C:\Program Files (x86)\Fenox V1.0\SisFnxUpdate.exe'; $s.Save()"
